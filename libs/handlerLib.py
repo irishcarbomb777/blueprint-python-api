0d1f7ae370a010aa8019e79d2996556c43a5ed28
irishcarbomb777 <danielclannan@gmail.com>
2020-12-28 18:33:06 -0500

    Add comments

--- libs/handlerLib.py
+++ libs/handlerLib.py
@@ -8,6 +8,8 @@ def handler(fn):
         statusCode=200
         return body, statusCode
     body, statusCode = func()
+    # This below function is required to deal with json packages lack of decimal
+    # handling (dynamodb returns all numbers as decimals)
     def replace_decimals(items):
         if isinstance(items, list):
             return [replace_decimals(i) for i in items]
@@ -16,7 +18,9 @@ def handler(fn):
         elif isinstance(items, decimal.Decimal):
             return int(items) if items % 1 == 0 else items
         return items
-
+    # This below function is required to convert the items object to lists as opposed
+    # to sets. Json is a js based function and does not know how to deal with
+    # set type objects
     def set_default(obj):
         if isinstance(obj, set):
             return(list(obj))
